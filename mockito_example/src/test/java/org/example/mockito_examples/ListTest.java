d91c806dc403f12536db2380c3a1171986b98b83
Vishal Singh <vishalkumar70854@gmail.com>
2024-05-25 21:27:35 +0530

    learn and implemented verify() method in Mockito

--- mockito_example/src/test/java/org/example/mockito_examples/ListTest.java
+++ mockito_example/src/test/java/org/example/mockito_examples/ListTest.java
@@ -6,8 +6,7 @@ import org.mockito.Mockito;
 
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ListTest {
 
@@ -55,6 +54,44 @@ public class ListTest {
         Mockito.when(listMock.get(Mockito.anyInt())).thenThrow(new RuntimeException("List is empty"));
         listMock.get(0);
 
+    }
+
+    // testing verify() method in Mockito
+    /*
+    Once a mock or spy has been used, we can verify that specific interactions took place.
+    Literally, we are saying, “Hey, Mockito, make sure this method was called with these arguments.”
+     */
+    @Test
+    public void letsMockList_ByVerifyMethod(){
+        List list = mock(List.class);
+        when(list.get(0)).thenReturn("Hello Vishal");
+        String expected = "Hello Vishal";
+        Assert.assertEquals(expected, list.get(0));
+
+        // Mockito.verify(mock variable ex: list).methodName(arguments) // arguments need to be same as used under when() method
+        // verify() method will check if the above mock variable's method i.e get() is called with argument 0
+        // i.e list.get(0) if this method is called then verify will run the test else the test will fail if list.get(2) (if get() method is called with some other argument)
+        verify(list).get(0);
+
+        // By default, Mockito verifies that the method was called once, but you can verify any number of invocations:
+        // verify the exact number of invocations
+//        verify(list, times(42)).get(anyInt());
+//
+//        // verify that there was at least one invocation
+//        verify(list, atLeastOnce()).get(anyInt());
+//
+//        // verify that there were at least 5 invocations
+//        verify(list, atLeast(5)).get(anyInt());
+//
+//        // verify the maximum number of invocations
+//        verify(list, atMost(5)).get(anyInt());
+//
+//        // verify that it was the only invocation and
+//        // that there are no more unverified interactions
+//        verify(list, only()).get(anyInt());
+//
+//        // verify that there were no invocations
+//        verify(list, never()).get(anyInt());
     }
 
 }
